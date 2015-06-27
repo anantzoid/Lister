6c392c0e1a9ebf2bed532ab865e84ce47cd67b4b
anantzoid <anant718@hotmail.com>
2015-06-27 11:32:46 +0530

    delete link and basic styling

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -11,13 +11,14 @@ angular.module('listerApp')
   .controller('MainCtrl', function ($scope, $http, Links) {
        
        $scope.addNewSection = false; 
-
+       $scope.edit = false;
        //TODO put in commonplace
        $scope.newRecord = {
            title: '',
            link: ''
        };
        $scope.Links = Links;
+
 
        $scope.links = [
             {
@@ -36,34 +37,31 @@ angular.module('listerApp')
 
         $scope.saveLink = function() {
 
-           $http.get($scope.newRecord.link)
-           .then(function(data) { 
 
                //TODO stautus validation
                 
-            console.log(data); 
-               var matches = data.data.match(/<title>(.*?)<\/title>/);
-               console.log(matches);
-                $scope.newRecord.title = matches[0];
 
-                var save = $scope.Links.$add({
-                    title: $scope.newRecord.title,
-                    link: $scope.newRecord.link
-                });        
+            var save = $scope.Links.$add({
+                title: $scope.newRecord.title,
+                link: $scope.newRecord.link
+            });        
 
 
-                $scope.newRecord = {
-                    title: '',
-                    link: ''
-                };
+            $scope.newRecord = {
+                title: '',
+                link: ''
+            };
 
-                if(save) {
-                    console.log('success');
-                    $scope.addNewSection = false; 
-                } else {
-                    console.log('error');
-                }
+            if(save) {
+                console.log('success');
+                $scope.addNewSection = false; 
+            } else {
+                console.log('error');
+            }
 
-           });
+        };
+
+        $scope.removeLink = function(link) {
+            $scope.Links.$remove(link);
         };
   });
